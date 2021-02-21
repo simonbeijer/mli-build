4ecb4e0f661bf81ab57f1bf2a71ecaa0e82de625
Simon Beijer <beijer.simon@gmail.com>
2021-02-21 19:16:03 +0100

    Added about text

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -7,32 +7,34 @@ function About() {
       <div className="row">
         <div className="text-center">
           <div className="col-8 mx-auto">
-            <h1>Middle Lake Industries</h1>
-            <div className="col-3 mx-auto"></div>
+            <h1>How it all started</h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Repudiandae optio eveniet facilis error reiciendis quas molestias
-              culpa aliquid quo tenetur, dolore, quis itaque eius ratione ea
-              iusto omnis fugiat recusandae.
+              Flyfishing got me into baitbuilding. When my interest in fishing
+              grew bigger, I was actually more into flyfishing. I would still
+              rate flyfishing as the ultimate form of fishing. There’s just
+              something to it, it’s hard to explain, it’s the complete package.
+            </p>
+
+            <p>
+              I primarily wanted to fish after trout and around the place I grew
+              up in Sweden there wasn’t that many waters with good troutfishing,
+              but plenty of waters with good populations of pike and zander. So
+              due to the fact that it more convenient, trolling and spinfishing
+              for pike and zander became the primary fishing methods and
+              flyfishing took a step back.
             </p>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Repudiandae optio eveniet facilis error reiciendis quas molestias
-              culpa aliquid quo tenetur, dolore, quis itaque eius ratione ea
-              iusto omnis fugiat recusandae.
+              But the thing about flyfishing is that you often tie the flies you
+              fish with. In contrast when spinfishing, trolling or in general
+              when you’re not flyfishing, you tend to buy lures from the tackle
+              shop. I missed the feeling when catching fish on something built
+              by myself. The creative process and the fact that I could make a
+              lure exactly how I wanted it, those where things that got me
+              started with baitbuilding. So back in 2015 I made my first lures
+              and since then it’s one of my favorite things to do besides
+              fishing.
             </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Repudiandae optio eveniet facilis error reiciendis quas molestias
-              culpa aliquid quo tenetur, dolore, quis itaque eius ratione ea
-              iusto omnis fugiat recusandae.
-            </p>
-            <p>
-              Lorem ipsum dolor sits consectetur, adipisicing elit. Repudiandae
-              optio eveniet facilis error reiciendis quas molestias culpa
-              aliquid quo tenetur, dolore, quis itaque eius ratione ea iusto
-              omnis fugiat recusandae.
-            </p>
+            <p>/Nils</p>
           </div>
         </div>
       </div>
