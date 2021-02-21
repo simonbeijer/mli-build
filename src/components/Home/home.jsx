3ebe066a6e770a71c6e25499c8d7ecb8df988abe
Simon Beijer <beijer.simon@gmail.com>
2021-02-21 18:29:31 +0100

    New stuff added

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import './home.scss';
-import ControlledCarousel from '../Carousel/carousel'
-
-
+import React from "react";
+import "./home.scss";
+import ControlledCarousel from "../Carousel/carousel";
 
 function Home() {
-
   return (
     <main className="App-home container-fluid">
-      <ControlledCarousel className="carousel"/>
+      <ControlledCarousel className="carousel" />
       <div className="row">
-
         <div className="text-center">
           <div className="col-8 mx-auto">
-            <h1>Middle Lake Industries</h1>
-            <div className="col-3 mx-auto">
-            </div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae optio eveniet facilis error reiciendis quas molestias culpa aliquid quo tenetur, dolore, quis itaque eius ratione ea iusto omnis fugiat recusandae.         </p>
+            <h1 className="homeH1">Middle Lake Industries</h1>
+            <div className="col-3 mx-auto"></div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Repudiandae optio eveniet facilis error reiciendis quas molestias
+              culpa aliquid quo tenetur, dolore, quis itaque eius ratione ea
+              iusto omnis fugiat recusandae.{" "}
+            </p>
           </div>
         </div>
       </div>
@@ -26,6 +25,3 @@ function Home() {
 }
 
 export default Home;
-
-
-
