7c299beea24d3b332c0b51cbf7fac3bf379b8e2e
Simon Beijer <beijer.simon@gmail.com>
2021-03-05 16:42:00 +0100

    router test 6

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import About from "./components/About/about";
 import Products from "./components/Products/products";
 import Contact from "./components/Contact/contact";
 import ProductProvider from "./components/Context/productContext";
-import { BrowserRouter, Switch, Route, IndexRoute } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <ProductProvider>
       <div className="App">
-        <BrowserRouter basename="">
+        <BrowserRouter>
           <Header />
           <Switch>
-            <IndexRoute component={Home} />
+            <Route exact to="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/products" component={Products} />
             <Route path="/contact" component={Contact} />
