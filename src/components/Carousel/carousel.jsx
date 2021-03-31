2299a29d2a5544b2384a750463d090695b7b796f
Simon Beijer <beijer.simon@gmail.com>
2021-03-31 09:32:35 +0200

    Fixed emblem

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./carousel.scss";
-import logo from "../../assets/Emblem@4x.png";
+import logo from "../../assets/Emblem.png";
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
 
@@ -16,8 +16,14 @@ function ControlledCarousel() {
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <img
-            className="activ d-block img-fluid w-100 "
-            src="https://images.unsplash.com/photo-1554132273-851fc5eb4a2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80"
+            class="align-middle"
+            style={{
+              height: "350px",
+              width: "auto",
+              margin: "auto",
+            }}
+            className="activ d-block img-fluid"
+            src={logo}
             alt="First slide"
           />
         </Carousel.Item>
@@ -31,7 +37,7 @@ function ControlledCarousel() {
         <Carousel.Item>
           <img
             className="d-block img-fluid w-100 "
-            src={logo}
+            src="https://images.unsplash.com/photo-1554132273-851fc5eb4a2e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1767&q=80"
             alt="Third slide"
           />
         </Carousel.Item>
